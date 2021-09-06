adc7205baff17067fdbb409048c5435795ff4b46
Denise Giubilei <giubileidenise@gmail.com>
2021-09-06 17:03:02 -0300

    refactor: remove unused create http method and route

--- src/controllers/AnswersController.ts
+++ src/controllers/AnswersController.ts
@@ -3,22 +3,6 @@ import { Request, Response } from "express";
 import { AnswerService } from "../services/AnswerService";
 
 class AnswersController {
-  async create(request: Request, response: Response): Promise<Response> {
-    const { text } = request.body;
-
-    const answersService = new AnswerService();
-
-    try {
-      const answer = await answersService.create({ text });
-
-      return response.json(answer);
-    } catch (err) {
-      return response.status(400).json({
-        message: err.message,
-      });
-    }
-  }
-
   async list(request: Request, response: Response): Promise<Response> {
     const answersService = new AnswerService();
 
--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,6 @@ const routes = Router();
 
 const answersController = new AnswersController();
 
-routes.post("/answers", answersController.create);
 routes.get("/answers", answersController.list);
 
 routes.get("/ping", (_, res) => res.json({ pong: true }));
